16966639b122cd533c62bc7e668322a04515e53c
Filippo <filippomaria.cornacchia@studenti.isissgobetti.it>
2024-10-21 11:39:16 +0200

    3-sqlINDIANO

--- 3_select_from_nobel.sql
+++ 3_select_from_nobel.sql
@@ -5,45 +5,50 @@ FROM nobel
 WHERE yr = 1950;
 
 -- # 2. Show who won the 1962 prize for Literature.
+
 SELECT winner
-FROM nobel
-WHERE yr = 1962 
-AND subject = 'Literature';
+FROM nobel.nobel
+WHERE yr = 1962 AND subject = 'Literature';
 
 -- # 3. Show the year and subject that won 'Albert Einstein' his prize.
+
 SELECT yr, subject
-FROM nobel
+FROM nobel.nobel
 WHERE winner = 'Albert Einstein';
 
 -- # 4. Give the name of the 'Peace' winners since the year 2000,
 -- # including 2000.
+
 SELECT winner
-FROM nobel
-WHERE subject = 'Peace' 
-AND yr >= 2000;
+FROM nobel.nobel
+WHERE subject = 'Peace' AND yr >= 2000;
 
 -- # 5. Show all details (yr, subject, winner) of the Literature prize
 -- # winners for 1980 to 1989 inclusive.
+
 SELECT *
-FROM nobel
-WHERE subject = 'Literature'
-AND yr BETWEEN 1980 AND 1989;
+FROM nobel.nobel
+WHERE subject = 'Literature' AND yr BETWEEN 1980 AND 1989;
 
 -- # 6. Show all details of the presidential winners: ('Theodore
 -- # Roosevelt', 'Woodrow Wilson', 'Jed Bartlet', 'Jimmy Carter')
-SELECT yr, subject, winner
-FROM nobel
+
+SELECT *
+FROM nobel.nobel
 WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jed Bartlet', 'Jimmy Carter');
 
+
 -- # 7. Show the winners with first name John
+
 SELECT winner
-FROM nobel
+FROM nobel.nobel
 WHERE winner LIKE 'John%';
 
 -- # 8. In which years was the Physics prize awarded but no Chemistry prize.
-SELECT DISTINCT yr FROM nobel
-WHERE subject = 'Physics'
-AND yr NOT IN
-  (SELECT yr
-  FROM nobel
-  WHERE subject = 'Chemistry')
+
+SELECT yr
+FROM nobel.nobel
+WHERE subject = 'Physics' AND yr NOT IN
+(SELECT yr
+FROM nobel.nobel
+WHERE subject = 'Chemistry');
