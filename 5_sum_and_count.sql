ed67c653a5c4cf69552c9cfed4ae396bfc1954fb
Filippo <filippomaria.cornacchia@studenti.isissgobetti.it>
2024-10-25 09:24:07 +0200

    4-sqlINDIANO , 5-sqlINDIANO

--- 5_sum_and_count.sql
+++ 5_sum_and_count.sql
@@ -1,41 +1,41 @@
 -- # 1. Show the total population of the world.
 SELECT SUM(population)
-FROM world;
+FROM world.world;
 
 -- # 2. List all the continents - just once each.
 SELECT DISTINCT continent
-FROM world;
+FROM world.world;
 
 -- # 3. Give the total GDP of Africa
 SELECT SUM(gdp)
-FROM world
+FROM world.world
 WHERE continent = 'Africa';
 
 -- # 4. How many countries have an area of at least 1000000
 SELECT COUNT(name)
-FROM world
+FROM world.world
 WHERE area >= 1000000;
 
 -- # 5. What is the total population of ('France','Germany','Spain')
 SELECT SUM(population)
-FROM world
+FROM world.world
 WHERE name IN ('France', 'Germany', 'Spain');
 
 -- # 6. For each continent show the continent and number of countries.
-SELECT continent, COUNT(name)
-FROM world
+SELECT continet, COUNT(name)
+FROM world.world
 GROUP BY continent;
 
 -- # 7. For each continent show the continent and number of countries
 -- # with populations of at least 10 million.
 SELECT continent, COUNT(name)
-FROM world
+FROM world.world
 WHERE population >= 10000000
 GROUP BY continent;
 
 -- # 8. List the continents with total populations of at least 100
 -- # million.
 SELECT continent
-FROM world
+FROM world.world
 GROUP BY continent
 HAVING SUM(population) >= 100000000;
